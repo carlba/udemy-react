d1ceec872e0adc2727537149a1cf2a40b1f6ab36
Carl Bäckström <genzorg@gmail.com>
2021-01-24 16:21:54 +0100

    Convert functional component to class based

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div class="App">
-      <h1>Hi I'm a React App</h1>
-      <p>This is really working!</p>
-      <Person name="Carl" age="37" />
-      <Person name="Johanna" age="32">
-        My hobbies: Dancing
-      </Person>
-      <Person name="Tobias" age="27" />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div class="App">
+        <h1>Hi I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person name="Carl" age="37" />
+        <Person name="Johanna" age="32">
+          My hobbies: Dancing
+        </Person>
+        <Person name="Tobias" age="27" />
+      </div>
+    );
+  }
 }
 
 export default App;
