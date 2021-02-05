0839d71647e2cb4e6297a2dd0b1b2534efc985fc
Carl Bäckström <genzorg@gmail.com>
2021-02-05 10:58:07 +0100

    ### 242. Adding Custom Form Validation
    https://kutt.it/JWqvLQ

--- README.md
+++ README.md
@@ -1609,3 +1609,23 @@ https://kutt.it/3hnVmY
 * On submission an event handler is triggered
   * The data in the `this.state.orderForm` is converted to a format suitable for submission to server.
 
+### 242. Adding Custom Form Validation
+
+https://kutt.it/JWqvLQ
+
+```jsx
+checkValidity(value, rules) {
+  let isValid = true;
+  if (rules.required) {
+    isValid = value.trim() !== '' && isValid;
+  }
+  if (rules.minLength) {
+    isValid = value.length >= rules.minLength && isValid;
+  }
+  if (rules.maxLength) {
+    isValid = value.length <= rules.maxLength && isValid;
+  }
+  return isValid;
+}
+```
+
--- burger-builder/src/containers/ContactData/ContactData.js
+++ burger-builder/src/containers/ContactData/ContactData.js
@@ -17,7 +17,9 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Name'
         },
-        value: ''
+        value: '',
+        validation: { required: true },
+        valid: false
       },
       street: {
         elementType: 'input',
@@ -25,7 +27,9 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Street'
         },
-        value: ''
+        value: '',
+        validation: { required: true },
+        valid: false
       },
       country: {
         elementType: 'input',
@@ -33,7 +37,9 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Country'
         },
-        value: ''
+        value: '',
+        validation: { required: true },
+        valid: false
       },
       zipCode: {
         elementType: 'input',
@@ -41,7 +47,9 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Zip Code'
         },
-        value: ''
+        value: '',
+        validation: { required: true, minLength: 5, maxLength: 5 },
+        valid: false
       },
       email: {
         elementType: 'input',
@@ -49,7 +57,9 @@ class ContactData extends Component {
           type: 'email',
           placeholder: 'Your Email'
         },
-        value: ''
+        value: '',
+        validation: { required: true },
+        valid: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -91,13 +101,35 @@ class ContactData extends Component {
     }
   };
 
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+    return isValid;
+  }
+
   handleInputChange = (event, inputId) => {
-    this.setState((prevState, props) => ({
-      orderForm: {
-        ...prevState.orderForm,
-        [inputId]: { ...prevState.orderForm[inputId], value: event.target.value }
-      }
-    }));
+    this.setState((prevState, props) => {
+      const updatedForm = {
+        orderForm: {
+          ...prevState.orderForm,
+          [inputId]: {
+            ...prevState.orderForm[inputId],
+            value: event.target.value,
+            valid: this.checkValidity(event.target.value, prevState.orderForm[inputId].validation)
+          }
+        }
+      };
+      console.log(updatedForm);
+      return updatedForm;
+    });
   };
 
   render() {
