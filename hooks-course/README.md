f6fcf90872dc02725fea8ebd7c1999a4748ecccf
Carl Bäckström <genzorg@gmail.com>
2021-02-07 17:18:59 +0100

    442. Loading Errors & State Batching

--- hooks-course/README.md
+++ hooks-course/README.md
@@ -110,3 +110,33 @@ https://kutt.it/A9dLV8
 ### 441. Deleting Ingredients
 
 https://kutt.it/SbedDZ
+
+### 442. Loading Errors & State Batching
+
+https://kutt.it/19rbH1
+
+![image-20210207171850107](https://cdn.jsdelivr.net/gh/carlba/assets@master/GigKTE-image-20210207171850107.png)
+
+React batches state updates - see: https://github.com/facebook/react/issues/10231#issuecomment-316644950
+
+That simply means that calling
+
+```
+setName('Max');setAge(30);
+```
+
+in the same synchronous (!) execution cycle (e.g. in the same function) will **NOT trigger two component re-render cycles**.
+
+Instead, the component will **only re-render once** and both state updates will be **applied simultaneously**.
+
+Not directly related, but also sometimes misunderstood, is when the new state value is available.
+
+Consider this code:
+
+```
+console.log(name); // prints name state, e.g. 'Manu'setName('Max');console.log(name); // ??? what gets printed? 'Max'?
+```
+
+You could think that accessing the name state after `setName('Max');` should yield the new value (e.g. `'Max'`) but this is **NOT the case**. Keep in mind, that the **new state value is only available in the next component render cycle** (which gets scheduled by calling `setName()`).
+
+**Both concepts (batching and when new state is available) behave in the same way for both functional components with hooks as well as class-based components with** `**this.setState()**`**!**
--- hooks-course/src/components/Ingredients/Ingredients.js
+++ hooks-course/src/components/Ingredients/Ingredients.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState, useCallback } from 'react';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 
 export function convertObjectToArray(obj) {
   if (!obj) {
@@ -15,31 +16,55 @@ export function convertObjectToArray(obj) {
 
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const handleFilterChange = useCallback(filteredIngredients => {
     setIngredients(filteredIngredients);
   }, []);
 
   const handleAddIngredient = async ingredient => {
-    const response = await fetch('https://udemy-react-hooks-course-default-rtdb.firebaseio.com/ingredients.json', {
-      method: 'POST',
-      body: JSON.stringify(ingredient),
-      headers: { 'Content-Type': 'application/json' }
-    });
-    const json = await response.json();
-    setIngredients(prevIngredients => [...prevIngredients, { ...ingredient, id: json.name }]);
+    setIsLoading(true);
+    let json;
+    try {
+      const response = await fetch('https://udemy-react-hooks-course-default-rtdb.firebaseio.com/ingredients.json', {
+        method: 'POST',
+        body: JSON.stringify(ingredient),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      json = await response.json();
+    } catch (error) {
+      setError(error.message);
+      setIsLoading(false);
+    }
+    if (json) {
+      setIsLoading(false);
+      setIngredients(prevIngredients => [...prevIngredients, { ...ingredient, id: json.name }]);
+    }
   };
 
   const handleRemoveIngredient = async ingredientId => {
-    await fetch(`https://udemy-react-hooks-course-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`, {
-      method: 'DELETE'
-    });
+    setIsLoading(true);
+    try {
+      await fetch(`https://udemy-react-hooks-course-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`, {
+        method: 'DELETE'
+      });
+    } catch (error) {
+      setError(error.message);
+      setIsLoading(false);
+    }
+    setIsLoading(false);
     setIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingredientId));
+  };
+
+  const handleErrorModalOnClose = () => {
+    setError(null);
   };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={handleAddIngredient} />
+      {error ? <ErrorModal onClose={handleErrorModalOnClose}>{error}</ErrorModal> : null}
+      <IngredientForm onAddIngredient={handleAddIngredient} isLoading={isLoading} />
 
       <section>
         <Search onFilterChange={handleFilterChange} />
